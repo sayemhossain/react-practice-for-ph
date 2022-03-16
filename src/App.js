0602a30a83d4540b27e0e5a989748470f32ec21b
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-03-16 23:38:41 +0600

    new code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import Countries from "./components/Countries/Countries";
 
-const name = "Sayem Hossain";
 function App() {
   return (
     <div className="App">
-      <LoadCountries></LoadCountries>
-    </div>
-  );
-}
-
-function LoadCountries() {
-  const [countries, setCountries] = useState([]);
-
-  useEffect(() => {
-    fetch("https://restcountries.com/v3.1/all")
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  }, []);
-
-  return (
-    <div>
-      <h1>Visiting every country of the wrold!</h1>
+      <Countries></Countries>
     </div>
   );
 }
