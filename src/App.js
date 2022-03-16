a063d9be19cd510d78234e64215d40570dfbcc45
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-03-16 21:51:51 +0600

    new

--- src/App.js
+++ src/App.js
@@ -4,7 +4,19 @@ import { useState } from "react";
 
 const name = "Sayem Hossain";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <LoadCountries></LoadCountries>
+    </div>
+  );
+}
+
+function LoadCountries() {
+  return (
+    <div>
+      <h1>Visiting every country of the wrold!</h1>
+    </div>
+  );
 }
 
 export default App;
